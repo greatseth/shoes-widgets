0db466f8e2a51c7b767b4cf2fea656828cc663c9
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2008-11-25 11:36:40 -0500

    workaround the fact that radio.checked= and check.checked= do not call the click event handler

--- labeled_check.rb
+++ labeled_check.rb
@@ -3,7 +3,7 @@ class LabeledCheck < Shoes::Widget
     style :width => options.delete(:width)
     @check = check &block
     para text, options
-    click { @check.checked = !@check.checked? }
+    click { @check.checked = !@check.checked?; block.call }
   end
   
   def method_missing m,*a,&b
--- labeled_radio.rb
+++ labeled_radio.rb
@@ -3,7 +3,7 @@ class LabeledRadio < Shoes::Widget
     style :width => options.delete(:width)
     @radio = radio &block
     para text, options
-    click { @radio.checked = !@radio.checked? }
+    click { @radio.checked = !@radio.checked?; block.call }
   end
   
   def method_missing m,*a,&b
